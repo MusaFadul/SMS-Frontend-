470a5148ca4458e0040747a50f0d958bce6f4c1a
MusaFadul <musamokhtar2@gmail.com>
2019-05-22 13:31:57 +0200

    9

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faBars } from '@fortawesome/free-solid-svg-icons'
 import { Row, Col } from 'antd'
-import headerStyle from '../styles/header'
+
 
 
 
@@ -15,16 +15,16 @@ class Header extends Component {
   
     return (
 
-      <div style={{headerStyle}}> 
-                <div  className="faBars">         
+      <div style={{width:"100%",height: "80px",display: "flex",backgrounColor:"red",padding: "20px",border: "1px solid #efeaea",marginBottom: "10px"}}> 
+                <div style={{marginLeft:"20%"}}>         
                       <a href="/"> <FontAwesomeIcon icon={faBars}  size="3x"   className="navItem"/></a>
                   </div>     
-                  <div  className="title">
+                  <div   style={{marginLeft:"20%"}}>
                   
                       <h4 style={{fontWeight: "bold"}}>SMS Coding Challenge</h4>
                    
                   </div>
-                  <div className=" user_area">
+                  <div style={{marginLeft:"20%"}}>
                    
                       <a href="/form">  <FontAwesomeIcon icon={faUser} size="3x"  className="navItem"/> </a>      
                     </div>           
