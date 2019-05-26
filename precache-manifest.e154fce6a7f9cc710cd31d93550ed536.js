7d5d9ef2e213befa9651b7f6302ebedf34b5cc98
MusaFadul <musamokhtar2@gmail.com>
2019-05-26 16:11:34 +0200

    Updates

---
+++ precache-manifest.e154fce6a7f9cc710cd31d93550ed536.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e898b114d15f4ff2129c7109050c55f9",
+    "url": "/SMS-Frontend/index.html"
+  },
+  {
+    "revision": "65172d99166ca6df093f",
+    "url": "/SMS-Frontend/static/css/2.4df3d4d4.chunk.css"
+  },
+  {
+    "revision": "65172d99166ca6df093f",
+    "url": "/SMS-Frontend/static/js/2.340cd966.chunk.js"
+  },
+  {
+    "revision": "902c518ddbd1dda3919c",
+    "url": "/SMS-Frontend/static/js/main.a5cfb675.chunk.js"
+  },
+  {
+    "revision": "85e17bdd877347380b06",
+    "url": "/SMS-Frontend/static/js/runtime~main.4677ae6d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SMS-Frontend/precache-manifest.4d5b861cac15124833e482784a0697f9.js"
+  "/SMS-Frontend/precache-manifest.e154fce6a7f9cc710cd31d93550ed536.js"
 );
 
 self.addEventListener('message', (event) => {
